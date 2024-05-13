1f700a352c66c28d0f5905cfc843a8e1945fd05e
praveenswork <praveenpraveen25720@gmail.com>
2024-05-13 23:20:09 +0530

    Feat : Files added

---
+++ lesson5-exercise/app.js
@@ -0,0 +1,23 @@
+//Variables
+//5a
+let my_name = "Praveen";
+
+//5b
+console.log(`My name is : $(my_name)`);
+//5c
+let coffe = 5;
+let bagels = 5;
+let soup = 9;
+let cost = coffe + bagels * 2 + soup;
+//5d
+console.log(`Cost of food : ${cost}`);
+//5e
+tax = cost * 0.1;
+//5f
+console.log(`Tax[10%] : ${tax}`);
+//5g
+let totcost = tax + cost;
+console.log(`Total Cost : ${totcost}`);
+//5k
+
+//5l
