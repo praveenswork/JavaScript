5a8754cff3de557e6921ddef087add1178bd4d24
praveenswork <praveenpraveen25720@gmail.com>
2024-05-28 23:44:38 +0530

    Feat : files modified

--- lesson10-HTMLCSS/exercise_10.js
+++ lesson10-HTMLCSS/exercise_10.js
@@ -1,1 +1,8 @@
-
+document.querySelector(".toogle").addEventListener("click", function () {
+  let element = document.querySelector(".toogle");
+  if (element.classList.contains("is-toogle")) {
+    element.classList.remove("is-toogle");
+  } else {
+    element.classList.add("is-toogle");
+  }
+});
