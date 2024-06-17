5f51c03a61ac698080cbdfa3da341e0586309843
praveenswork <praveenpraveen25720@gmail.com>
2024-06-17 18:52:30 +0530

    Make the cart interactive

--- lesson13-AmazonProject_JS/javascript-amazon-project-main/amazon.js
+++ lesson13-AmazonProject_JS/javascript-amazon-project-main/amazon.js
@@ -21,7 +21,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-price">
-            ${(product.priceCents / 100).toFixed(2)}
+            $${(product.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -46,10 +46,36 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-addcart" data-product-id='${
+            product.id
+          }'>
             Add to Cart
           </button>
         </div>`;
 });
 
 document.querySelector(".js-products").innerHTML = productHTML;
+
+document.querySelectorAll(".js-addcart").forEach((button) => {
+  button.addEventListener("click", () => {
+    const productId = button.dataset.productId;
+
+    let matchingItem;
+
+    cart.forEach((item) => {
+      if (item.productId === productId) {
+        matchingItem = item;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1,
+      });
+    }
+    console.log(cart);
+  });
+});
