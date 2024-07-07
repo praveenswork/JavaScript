8f74152bc333e9c1354a0405eefa0d486986cf4f
praveenswork <praveenpraveen25720@gmail.com>
2024-07-07 16:34:31 +0530

    construct in class created

--- lesson16_JasmineTesting/amazon_exercise/scripts/checkout.js
+++ lesson16_JasmineTesting/amazon_exercise/scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import "../data/cart_OOP.js";
+import "../data/cart_class.js";
 
 renderOrderSummary();
 renderPaymentSummary();
